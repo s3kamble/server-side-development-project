1e2fa4a018cd371c18beb9fe6216dd230efd9ba0
Shreyak <shreyak438@gmail.com>
2020-08-19 20:36:17 +0530

    Passport JWT

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,7 +8,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
+var config = require('./config');
 
 
 var indexRouter = require('./routes/index');
@@ -19,7 +19,7 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;//instead of auth-token
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -49,22 +49,6 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
