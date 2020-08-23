4cb5bf6f32fa5b9e4596771d6abee33ae0e96883
Shreyak <shreyak438@gmail.com>
2020-08-23 15:01:02 +0530

    oAuth and favourite router added

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,80 +1,107 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
-const mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favoriteRouter');
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
 
-const url = config.mongoUrl;//instead of auth-token
-const connect = mongoose.connect(url);
+const url = config.mongoUrl;
+
+const connect = mongoose.connect(url, {
+  useMongoClient: true
+});
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  console.log('Connected to server!');
+}, (err) => {
+  console.log(err);
+});
 
 var app = express();
+
+app.all('*', (req, res, nxt) => {
+  if(req.secure) {
+    return nxt();
+  } else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Secure traffic only
-// app.all('*', (req, res, next) => {
-//   if (req.secure) {
-//     return next();
-//   }
-//   else {
-//     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-//   }
-// });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
   name: 'session-id',
-  secret: '12345-67890-09876-54321',
+  secret: 'Jesus-Loves-w@ch!ra-so-Much!!',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }));
-
+// app.use(session({secret: 'SECRET'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/imageUpload',uploadRouter);
+app.use('/', index);
+app.use('/users', users);
 
+// authentication before resource access
+// function auth(req, res, next) {
+//   // console.log(req.signedCookies);
+//   console.log(req.session);
 
-
+//   // if(!req.signedCookies.user){
+//   if(!req.user){  
+//     // var authHeader = req.headers.authorization;
+//     // if(!authHeader){
+//       var err = new Error('You are not authenticated!');
+//       // res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 403;
+//       return next(err);
+//     }
+//     else {
+//       // if(req.signedCookies.user === 'admin'){ 
+//       next();     
+//     }
+// }
+// app.use(auth);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
 
-
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -77,6 +77,15 @@ router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req,
   });
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
 
 router.get('/logout', (req, res, next) => {
   if (req.session) {
